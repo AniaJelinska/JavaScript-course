d3748fa963316dc35f8d5612de43c090581a497d
Ania Jelińska <ania.jelin@wp.pl>
2019-05-12 22:04:16 +0200

    lesson - constructor functions

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
-var myCar2 = {
+var Car = function(maxSpeed, driver){
 
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){ 
+    this.maxSpeed = maxSpeed;
+    this.driver - driver;
+    this.drive = function(speed, time){
         console.log(speed * time);
-    },
-    logDriver: function(){
+    };
+    this.logDriver = function(){
         console.log("driver name is " + this.driver);
-    }
+    };
 
-};
+}
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+var myCar = new Car (70, "Ninja Man");
+var myCar2 = new Car (70, "Humpty Dumpty");
+var myCar3 = new Car (70, "Shaun");
+var myCar4 = new Car (70, "James Bond");
+
+myCar.drive(30,50);
+myCar3.logDriver();
